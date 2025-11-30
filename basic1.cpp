f34159a4417172e920a3dbcc70a38e112c58cef5
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2025-11-30 13:10:57 +0530

    Add basic C++ program structure with comments
    Initial implementation of basic C++ program with commented code.

---
+++ basic1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include<string>
+using namespace std;
+int main(){
+
+
+
+
+// cout<<"Hello world  Amrendra "<<endl;
+    // int a,b;
+    // cin>>a>>b;
+
+    // cout<<a<<endl<<b<<endl;
+
+    // if(a>b){
+    //     cout<<"A is greater than B "<<a<<endl;
+
+    // }else{
+    //     cout<<"B is greater than A  "<<b<<endl;
+    //}
+
+
+    // for(int i=1;i<6;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout<<i;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(int i=0;i<5;i++){
+    //     for(int j=5;j>i;j--){
+
+    //         cout<<5-j+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+}
