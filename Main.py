d639c46d19f19e974c3f79067399e3f4cb490bc1
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-07-05 07:24:45 +0530

    Basics
    Basics of python

---
+++ Main.py
@@ -0,0 +1,78 @@
+# Welcome to Python Compiler! Press Ctrl+Enter to run
+#print("JAI SHREE RAM")
+#num1=int(input("Enter the first number "))
+#num2=int(input("Enter the second number"))
+
+#num3=num2+num1
+
+#EVEN NUMBER IN PYTHON
+#if(num3%2==0):
+ # print("Yes Even")
+#else:
+ # print("No ODD")
+
+#for i in range(1,100):
+ # if (i%2!=0):
+  #  print(i)
+
+
+
+#number between 1 to 100 which are divisble by 3 and 5
+
+#for i in range(1,101):
+ # if((i%3==0)and (i%5==0)):
+  #  print(i)
+
+#for i in range (1,101):
+ # if(i%3==0) or (i%5==0):
+  #  print(i)
+
+
+#find out entered number is prime or not 
+
+#num1=int(input("Enter the number "))
+
+#print(num1)
+
+ 
+# nums=[4,4,21,2,1,110]
+# for num in nums:
+#   if num%2==0:
+#     print(num)
+
+# list1 = [10, 21, 4, 45, 66, 93] 
+
+# for nums in list1:
+#   if nums%2!=0:
+#     print(nums)
+
+
+2342
+
+i= int(input("Enter the number "))
+sum=0
+
+n=i
+
+while (n>0):
+  i=n%10
+  sum=sum+i
+  n=n//10
+
+
+print(sum)
+  
+  
+  
+    
+    
+
+
+
+
+
+
+    
+
+
+
