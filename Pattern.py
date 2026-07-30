3cd4fa9273e5e28d72769a08bb2bca84f71e14ce
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-07-30 06:59:51 +0530

    Comment out star pattern printing loop
    Comment out the nested loop for printing stars.

--- Pattern.py
+++ Pattern.py
@@ -2,10 +2,10 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 
 
-for i in range (0,5):
-    for j in range(0,5):
-        print("*",end="")
-    print()
+# for i in range (0,5):
+#     for j in range(0,5):
+#         print("*",end="")
+#     print()
 # *****
 # *****
 # *****
