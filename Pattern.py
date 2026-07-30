7be334807cb9905b39284d13bb2e4aa48645de9c
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-07-30 06:59:21 +0530

    Create Pattern.py for printing patterns
    Added a pattern printing script in Python.

---
+++ Pattern.py
@@ -0,0 +1,34 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+
+for i in range (0,5):
+    for j in range(0,5):
+        print("*",end="")
+    print()
+# *****
+# *****
+# *****
+# *****
+# *****
+
+
+# for i in range (0,5):
+#     for j in range (0,i+1):
+#         print("*",end=" ")
+#     print()
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+
+
+# for i in range (1,5):
+#     for j in range (1,i+1):
+#         print(j,end="")
+#     print()
+# 1
+# 12
+# 123
+# 1234
