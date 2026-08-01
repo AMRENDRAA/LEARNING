1c2bbecfc12ae413b2e88855c7c9bdf54ea77aa4
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-08-01 07:55:50 +0530

    Remove commented-out code and add print statement

--- Pattern.py
+++ Pattern.py
@@ -2,6 +2,9 @@
 # Online Python - IDE, Editor, Compiler, Interpreter
 
 
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+
 # for i in range (0,5):
 #     for j in range(0,5):
 #         print("*",end="")
@@ -31,4 +34,55 @@
 # 1
 # 12
 # 123
+
 # 1234
+
+# size = int(input("Enter the size of the array: "))
+# l = []
+
+# for i in range(0, size):
+#     # Take input into a unique variable
+#     num = int(input(f"Enter element {i+1}: "))
+#     # Append directly to the list without re-assigning l
+#     l.append(num)
+
+# # Use Python's built-in sum function instead of a second loop
+# print("The sum is:", sum(l))
+# sum=0;
+# for i in range (0,size):
+#     sum=sum+l[i]
+    
+# print("The sum is ",sum)
+
+
+# n=int(input())
+# c=0
+# while(n>0):
+    
+#     n=n//10
+#     c += 1
+# print(c)
+
+
+
+print("*"*5);
+
+# REVERSE OF A NUMBER 
+
+
+n=0 
+i=int(input("Enter the number "))
+j=i;
+
+
+while(j>0):
+    j=j%10
+
+
+
+        
+        
+        
+        
+    
+    
