3e9fe6cc7983f719d2da448af4050857860a7907
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-08-22 17:08:30 +0530

    Implement countOddEven and findMean methods
    Added methods to count odd and even numbers and to calculate the mean of an array.

--- basic20.py
+++ basic20.py
@@ -1,1 +1,26 @@
+Count Odd and Even
+
+class Solution:
+	def countOddEven(self, arr):
+	    count1=0
+	    count2=0
+	    length=len(arr)
+	    for i in range(length):
+	        if arr[i]%2==0:
+	            count1=count1+1
+	        else:
+	            count2=count2+1
+	    return(count2,count1)
+
+
+Mean or Average of an Array
+class Solution:
+    def findMean(self, arr):
+        sum=0
+        n=len(arr)
+        for i in range(n):
+            sum=sum+arr[i]
+        
+        
+        return sum//n  
 
