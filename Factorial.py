d325184c166966578a3ada6b14d9b82aad343178
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-09-22 07:50:40 +0530

    Implement Fibonacci function in Factorial.py
    Added a function to calculate Fibonacci numbers.

--- Factorial.py
+++ Factorial.py
@@ -53,5 +53,13 @@ if(sum==0):
     print("yes prime ")
 else:
     print("no not prime")
+#N TH FIBONACCI
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(9))
     
 
