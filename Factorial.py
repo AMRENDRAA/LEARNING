2ee650389815a159aca63c4da08cc997ebd21c68
AMRENDRA RAJ <68627259+AMRENDRAA@users.noreply.github.com>
2026-09-22 07:31:50 +0530

    Revise factorial calculation with user input
    Refactored factorial calculation to use user input and a new variable.

--- Factorial.py
+++ Factorial.py
@@ -8,3 +8,16 @@ while (num>0):
     num=num-1
 
 print(prod)
+
+#REVISION 
+
+num=int(input("Enter the number"))
+ans=1
+
+while(num>0):
+    ans=ans*num
+    num=num-1
+    
+    
+    
+print(ans)
